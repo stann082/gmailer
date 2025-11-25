7e0775278d69f135b6d2802759d0d019f5decd3f
stann082 <sbennett08@pm.me>
2025-11-25 15:56:37 -0600

    Initial view of details

--- src/ui/Views/EmailListView.xaml.cs
+++ src/ui/Views/EmailListView.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace ui.Views;
+﻿using Serilog;
+
+namespace ui.Views;
+
 using Email = core.Email;
 
 public partial class EmailListView
@@ -16,5 +19,22 @@ public partial class EmailListView
     public Email[] GetSelectedEmails()
     {
         return EmailList.SelectedItems?.Cast<Email>().ToArray() ?? [];
+    }
+
+    private async void OnEmailDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        try
+        {
+            if (e.Parameter is not Email email)
+            {
+                return;
+            }
+
+            await Shell.Current.Navigation.PushAsync(new EmailDetailPage(email));
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Error displaying email detail");
+        }
     }
 }
